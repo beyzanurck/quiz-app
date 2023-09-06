2916dacee00e3b47aca8fae91ac84d2faa00cf4f
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-09-06 17:03:26 -0500

    fetches data using an api

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
     res.json({ message: "Test" });
+});
+
+app.get('/animals', (req, res) => {
+    fetch('https://opentdb.com/api.php?amount=5&category=27&difficulty=easy&type=multiple')
+    .then(response => {
+    return response.json()
+    })
+    .then(apiData => {
+    res.json(apiData);
+    })
 });
 
 app.listen(PORT, () => {
