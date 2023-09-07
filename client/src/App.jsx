ab9c764cea6f9f7b5f557f6c50c80d28dbaa9847
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-09-06 20:56:52 -0500

    gets the data from the backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
+function App() { 
+
+  const [data, setData] = useState('')
+
+  const getData = async () => {
+    const response = await fetch("http://localhost:3000/animals");
+    const responseData = await response.json();
+    console.log(responseData.results)
+
+    setData(responseData.results)
+  }
+
+
+  useEffect(() => {
+    getData();
+  }, [])
   
   return (
     <>
-      <h1>Test</h1>
+      <p>{(data !== undefined && data[0] !== undefined) ? data[0].question : ""}</p>
     </>
   )
 }
